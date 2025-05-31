96c31422b44dbeb2912c05f5b984cd22139f62ba
neatsolin <neatsolin98@gmail.com>
2025-05-31 11:06:25 +0700

    add all in Auth

--- src/Auth/Login.ts
+++ src/Auth/Login.ts
@@ -1,1 +1,8 @@
+import { User } from "./User";
 
+export function loginUser(user: User, email: string, password: string): void {
+    const result = user.login(email, password);
+    if (!result) {
+        console.log("Login failed. Try again.");
+    }
+}
