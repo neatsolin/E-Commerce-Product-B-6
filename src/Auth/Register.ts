52d931069f7ceefb5836dbb129910a4b8c994453
neatsolin <neatsolin98@gmail.com>
2025-06-03 16:26:13 +0700

    change code all Auth and some edit

--- src/Auth/Register.ts
+++ src/Auth/Register.ts
@@ -1,22 +1,6 @@
-
-import { Address } from '../Customer/Address';
-import { Customer } from '../Customer/Customer';
-import { User } from '../Customer/User';
+import { User } from "../Customer/User";
 
 
-export class Register {
-  users: User[];
-
-  constructor() {
-    this.users = [];
-  }
-
-  register(phoneNumber: string, address: Address, role: string): User {
-    const user = new User(role);
-    if (role === 'customer') {
-      user.customer = new Customer(phoneNumber, address);
-    }
-    this.users.push(user);
-    return user;
-  }
+export function registerUser(user: User, email: string, password: string): boolean {
+    return user.register(email, password);
 }
